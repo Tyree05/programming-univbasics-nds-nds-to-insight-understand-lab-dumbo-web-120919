3c5ee827c619d51412b1f51d403617b07f12d914
Tyree Williams <tyreewilliams515@gmail.com>
2019-11-26 21:16:03 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,14 +10,14 @@ def pretty_print_nds(nds)
 end
 puts pretty_print_nds(directors_database)
 
-def print_first_directors_movie_titles
-  row_index = 0
-  while row_index < directors_database.count do
-    column_index = 0
-    while column_index < directors_database[row_index].count do
-      return directors_database[row_index][column_index]
-      column_index += 1
-    end
-    row_index +=1
-  end
-end
+# def print_first_directors_movie_titles
+#   row_index = 0
+#   while row_index < directors_database.count do
+#     column_index = 0
+#     while column_index < directors_database[row_index].count do
+#       return directors_database[row_index][column_index]
+#       column_index += 1
+#     end
+#     row_index +=1
+#   end
+# end
